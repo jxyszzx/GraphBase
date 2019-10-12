d76971975c82a99dd280d9185d791fa0cb5c492a
jxyszzx <jxyszzx@163.com>
2019-10-12 14:13:37 +0800

    debugged scc for exp

--- Flash/algos as pregel/scc.cpp
+++ Flash/algos as pregel/scc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 #include <set>
 #include <queue>
 #include <cmath>
@@ -7,27 +8,47 @@
 #include "omp.h"
 
 // #define DEBUG
-// #define EXP
 
-using namespace std;
-
-#ifdef EXP
-clock_t t_start;
-
-void init_time() {
-    t_start=clock();
+std::map<std::string, clock_t> t_starts_;
+void init_time(std::string name) {
+    t_starts_[name] = clock();
 }
-
 void get_time_cost(std::string name) {
-    double t_cost = (double)(clock()-t_start)/CLOCKS_PER_SEC;
+    double t_cost = (double)(clock()-t_starts_[name])/CLOCKS_PER_SEC;
     printf("%s time cost: %lf\n", name.c_str(), t_cost);
 }
-#endif
+
+using namespace std;
 
 Graph g;
 vector<set<int> > graph, graph_reverse;
 vector<set<int> > scc;
 set<int> vertices_;
+
+int N;
+long long *pos;
+int* edges;
+
+void create_graph() {
+    N = g.links.size();
+
+    pos = new long long[N + 1];
+
+    long long cnt = 0;
+    for (int i = 0; i < N; ++i) {
+        pos[i] = cnt;
+        cnt += g.links[i].size();
+    }
+    pos[N] = cnt;
+
+    edges = new int[cnt];
+    cnt = 0;
+    for (auto nbr : g.links) {
+        for (auto v : nbr) {
+            edges[cnt++] = v;
+        }
+    }
+}
 
 inline void set_Inter(const set<int>& s1, const set<int>& s2, set<int>& s3) {
     set_intersection(s1.begin(),s1.end(),s2.begin(),s2.end(),inserter(s3,s3.begin()));
@@ -56,28 +77,48 @@ void init() {
     }
 }
 
+
+
+
+
 void trim(vector<set<int> >& input_graph, vector<set<int> >& transpose_graph) {
     queue<int> to_delete;
 
     for (auto v : vertices_) {
+// printf("%d -\n", v);
+// if (v >= input_graph.size()) {
+//     puts("what the hell - 1");
+// }
+// cout << v << endl;
         if (input_graph[v].empty()) {
+            // puts("cool");
             to_delete.push(v);
-            vertices_.erase(v);
         }
     }
 
+// puts("1");
     while (!to_delete.empty()) {
         int u = to_delete.front();
         to_delete.pop();
-        scc.push_back(set<int>{u});
 
+        vertices_.erase(u);
+        scc.push_back(set<int>{u});
+// puts("2");
         for (auto v : vertices_) {
-            input_graph[v].erase(u);            
-            if (input_graph[v].empty()) {
-                to_delete.push(v);
-                vertices_.erase(v);
+// if (v >= input_graph.size()) {
+//     puts("what the hell - 2");
+// }
+            if (!input_graph[v].empty()) {
+                input_graph[v].erase(u);            
+                if (input_graph[v].empty()) {
+                    to_delete.push(v);
+                }
             }
         }
+// puts("3");
+// if (u >= transpose_graph.size()) {
+//     puts("what the hell - 3");
+// }
         transpose_graph[u].clear();
     }
 }
@@ -88,6 +129,7 @@ void updateScc(const set<int>& verts) {
 }
 
 void bfs(const set<int>& cur_verts, const vector<set<int> >& g, int pivot, set<int>& ret_verts) {
+// puts("bfs in");
     queue<int> que;
 
     que.push(pivot);
@@ -104,6 +146,7 @@ void bfs(const set<int>& cur_verts, const vector<set<int> >& g, int pivot, set<i
             ret_verts.insert(v);
         }
     }
+// puts("bfs out");
 }
 
 void forward_back_algo(set<int> verts) {
@@ -183,37 +226,48 @@ void printSCC() {
 
 int main(int argc, char *argv[])
 {
-    string filename(argv[1]);
-    filename = "../datasets/" + filename;
-    g = Graph(false, filename);
+    init_time("read");
 
-#ifdef EXP
-    init_time();
+    if (argc == 2) {
+        g = Graph(false, atoi(argv[1]), "");
+    } else if (argc == 3) {
+        // assert(atoi(argv[1]) == 0);
+        string filename(argv[2]);
+        filename = "../datasets/" + filename;
+        g = Graph(false, 0, filename);
+    }
+    get_time_cost("read");
+
+#ifdef DEBUG   
+    g.display();
 #endif
 
     // Init
+// puts("init start");
     init();
-    trim(graph, graph_reverse);
-    trim(graph_reverse, graph);
+// puts("init end");
 
-#ifdef DEBUG
-    puts("After trim");
-    for (auto v : vertices_) {
-        printf("%d ", v);
-    }
-    puts("");
-#endif
+// for (auto v : vertices_) {
+//     cout << v << endl;
+// }
+// puts("end vertices");
+    trim(graph, graph_reverse);
+
+// puts("trim1 end");
+    trim(graph_reverse, graph);
+// puts("trim2 end");
     
+    create_graph();
+
+    init_time("algo");
     // Algo
+    omp_set_num_threads(32);
     forward_back_algo(vertices_);
 
-#ifdef DEBUG
-    printf("%d\n", scc.size());
-    // printSCC();
-#endif
+    get_time_cost("algo");
 
-#ifdef EXP
-    get_time_cost("scc");
+#ifdef DEBUG
+    printSCC();
 #endif
 
     return 0;
